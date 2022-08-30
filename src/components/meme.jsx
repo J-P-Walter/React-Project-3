067003b94159467902400bf412138fc5c9e22ef7
JP <j.p.walter@outlook.com>
2022-08-30 15:04:40 -0400

    Added file of links of images, will be replaces with api call. Attatched text to image using state. Css could use more cleanup (and experience)

--- src/components/meme.jsx
+++ src/components/meme.jsx
@@ -1,8 +1,36 @@
 import React, { useState } from "react";
+import memesData from "../memesData";
 
 function Meme() {
   function handleClick() {
     console.log("Click");
+  }
+
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+
+  function getMemeImage() {
+    const memesArray = allMemeImages.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => {
+      return {
+        ...prevMeme,
+        [name]: value,
+      };
+    });
   }
 
   return (
@@ -12,15 +40,26 @@ function Meme() {
           className="form--input font"
           type="text"
           placeholder="Top text"
+          value={meme.topText}
+          name="topText"
+          onChange={handleChange}
         ></input>
         <input
           className="form--input font"
           type="text"
           placeholder="Bottom text"
+          value={meme.bottomText}
+          name="bottomText"
+          onChange={handleChange}
         ></input>
-        <button type="button" className="font" onClick={handleClick}>
+        <button type="button" className="font" onClick={getMemeImage}>
           Generate new image 🖼
         </button>
+      </div>
+      <div className="meme">
+        <img src={meme.randomImage} className="meme--image" />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
       </div>
     </main>
   );
